10e198946a75d9448a7a478c5f297707d998140d
davidphamm <92117345+davidphamm@users.noreply.github.com>
2022-09-19 11:26:29 -0700

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,6 @@
+// Setting the scores and selecting HTML elements
+var userScore = 0;
+var computerScore = 0;
+const pScore = document.getElementById('userScore');
+const cScore = document.getElementById('computerScore');
+const buttons = document.querySeletorAll('');
